dec67b3a892a74366d09588b42caac8cae03a662
Jung Hyun Nam <jeonghyun.nam@devsisters.com>
2020-04-01 12:02:48 +0900

    Add GitHub Actions Pipeline

--- HttpServerSample/Build.cmd
+++ HttpServerSample/Build.cmd
@@ -5,9 +5,9 @@ if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
 if exist x64 rd /s /q x64
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x86
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x86
 
 :exit
 popd
